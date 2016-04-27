e8dfdcfda0a9bef9f94d275846f43aaec3f96cec
Shuo Chen <chausolaris@gmail.com>
2016-04-26 21:25:26 -0400

    Add bot and util.

---
+++ util.lua
@@ -0,0 +1,56 @@
+local LoopQueue = {}
+
+function LoopQueue:new(max_n)
+  max_n = math.max(1, max_n)
+  local o = {
+    _max_n = max_n,
+    _full = false,
+    _next = 1
+  }
+  setmetatable(o, self)
+  self.__index = self
+  return o
+end
+
+function LoopQueue:append(x)
+  if not self._full and self._next == self._max_n then
+    self._full = true
+  end
+  self[self._next] = x
+  self._next = self._next % self._max_n + 1
+end
+
+function LoopQueue:size()
+  return self._full and self._max_n or self._next - 1
+end
+
+function LoopQueue:at(i)
+  if self._full and i <= self._max_n then
+    return self[(self._next + i - 2) % self._max_n + 1]
+  elseif not self._full and i <= self._next - 1 then
+    return self[i]
+  end
+end
+
+function LoopQueue:array()
+  local result = {}
+  for i, v in self:xarray() do
+    result[#result + 1] = v
+  end
+  return result
+end
+
+function LoopQueue:xarray()
+  local i = 0
+  return function ()
+    i = i + 1
+    v = self:at(i)
+    if v then
+      return i, v
+    end
+  end
+end
+
+return {
+  LoopQueue = LoopQueue
+}
