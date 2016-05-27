106aa49de9f7b18b1bbe0318c09fea94b2abc529
Shuo Chen <chausolaris@gmail.com>
2016-05-26 21:24:04 -0400

    update q model. implement rmsprop.

--- mario_q_model.lua
+++ mario_q_model.lua
@@ -8,13 +8,22 @@ require "cudnn"
 require "mario_util"
 
 local _ENABLE_CUDA = true
-local _TRAIN_FREQ = 1  -- train every # steps
+local _TRAIN_FREQ = 4  -- train every # steps
 local _EXP_SAMPLING_FREQ = 1  -- sample experience every # steps
 -- TODO: increae to 1000000
-local _EXP_CACHE_CAPACITY = 2000  -- experience cache capacity
+local _EXP_CACHE_CAPACITY = 10000  -- experience cache capacity
 -- TODO: increase to 32
 local _MINIBATCH_SIZE = 16  -- minibatch size
+local _LEARNING_RATE = 1.0e-4
+
 local _DISCOUNT_FACTOR = 0.9
+local _TRAIN_EPS_SCHEDULE = {
+  start_eps = 1.0,
+  end_eps = 0.1,
+  num_steps = 100000,  -- TODO: increase to 1000000
+}
+local _TEST_EPS = 0.05
+
 local _SQUEUE_SIZE = 4  -- state queue size
 local _NUM_STICKY_FRAMES = 6  -- # sticky frames
 
@@ -61,15 +70,12 @@ function QModel:new(mode, load_from, save_to, log_file)
   else
     m = self:_convNetModel()
   end
-
-  if _ENABLE_CUDA then
-    m.model = m.model:cuda()
-  end
+  m.model = self:_toCuda(m.model)
 
   x, dx = m.model:getParameters()
   if not load_from then
-    x:zero()
-    -- x:copy((torch.randn(x:size()) * 0.1):cuda())
+    -- x:zero()
+    x:copy((torch.randn(x:size()) * 0.1):cuda())
   end
 
   local is_train = (mode == "train")
@@ -83,16 +89,18 @@ function QModel:new(mode, load_from, save_to, log_file)
     _is_train = is_train,
     _load_from = load_from,
     _save_to = save_to,
-    _m = m, -- model
-    _x = x, -- model parameters
-    _dx = dx, -- model grad parameters
+    _m = m,  -- model
+    _x = x,  -- model parameters
+    _dx = dx,  -- model grad parameters
+    -- running averge of mean square of dx
+    _avg_ms_dx = (torch.ones(dx:size()) * 1.0e-6):cuda(),
 
-    _eps = 1.0,  -- eps greedy
-    _learning_rate = 0.0001,  -- learning rate
+    _eps = (is_train and _TRAIN_EPS_SCHEDULE.start_eps or _TEST_EPS),
 
     _epoch = 0,
     _step = 0,
     _current_state = nil,
+    _current_reward = 0.0,
     _exp_cache = mario_util.LoopQueue:new(_EXP_CACHE_CAPACITY),
     _num_saves = 0,
     _log_file = log_file
@@ -118,6 +126,7 @@ end
 function QModel:startEpoch(squeue)
   self._epoch = self._epoch + 1
   self._current_state = self:_zipState(squeue)
+  self._current_reward = 0
 end
 
 function QModel:_zipState(squeue)
@@ -126,15 +135,19 @@ function QModel:_zipState(squeue)
     local s = squeue[i]  -- state = (a0, r1, s1)
     if s then
       local screen = self._m.stdGameScreen(s[3])
-      if _ENABLE_CUDA then
-	screen = screen:cuda()
-      end
       ss[#ss + 1] = screen
     else
       ss[#ss + 1] = ss[#ss]
     end
   end
   return torch.cat(ss, 1)
+end
+
+function QModel:_toCuda(x)
+  if x and _ENABLE_CUDA then
+    return x:cuda()
+  end
+  return x
 end
 
 function QModel:_evalQ(s, a)
@@ -156,11 +169,12 @@ function QModel:_randomAction()
 end
 
 function QModel:_greedyAction()
-  local max_q, max_a = self:_maxQ(self._current_state)
+  local max_q, max_a = self:_maxQ(self:_toCuda(self._current_state))
   return max_a
 end
 
 function QModel:selectAction()
+  print("eps = "..self._eps)
   local a = nil
   if torch.uniform() < self._eps then
     print("Explore action:")
@@ -170,6 +184,50 @@ function QModel:selectAction()
     a = self:_greedyAction()  
   end
   return mario_util.joypad_input_code_from_simple[a]
+end
+
+function QModel:_updateEps()
+  if self._is_train and self._exp_cache:isFull() then
+    local sched = _TRAIN_EPS_SCHEDULE
+    self._eps = math.max(
+      sched.end_eps,
+      self._eps - (sched.start_eps - sched.end_eps) * 1.0 / sched.num_steps)
+  end
+end
+
+function QModel:_doTraining()
+  print("Do training...")
+
+  local samples = mario_util.randSample(
+    _MINIBATCH_SIZE, self._exp_cache:size())
+
+  local sum_dx = torch.zeros(self._dx:size()):cuda()
+  for i, sample in ipairs(samples) do
+    local exp = self._exp_cache:at(sample)
+    local s, a, r, sp = exp[1], exp[2], exp[3], exp[4]
+    s = self:_toCuda(s)
+    sp = self:_toCuda(sp)
+    local target_q = r
+    if sp then
+      target_q = r + _DISCOUNT_FACTOR * self:_maxQ(sp)
+    end
+
+    local q = self:_evalQ(s, a)  -- *
+    local dq = target_q - q
+
+    local grad_output = self:_toCuda(torch.zeros(
+      #mario_util.joypad_input_code_from_simple):float())
+    grad_output[a] = 1
+
+    self._dx:zero()
+    -- m.model:forward() has been called at *
+    self._m.model:backward(s, grad_output)
+    sum_dx:add(self._dx)
+  end
+
+  self._avg_ms_dx = self._avg_ms_dx * 0.9 + torch.pow(sum_dx, 2) * 0.1
+  sum_dx:cdiv(torch.sqrt(self._avg_ms_dx))
+  self._x:add(sum_dx * _LEARNING_RATE)
 end
 
 function QModel:feedback(squeue, mario_dies, level_clear)
@@ -184,54 +242,20 @@ function QModel:feedback(squeue, mario_dies, level_clear)
   -- update current state to be the next state
   self._step = self._step + 1
   self._current_state = sp
+  self._current_reward = self._current_reward + r
+  self:_updateEps()
   
   if not self._is_train then
     return
-  end
-
-  -- TODO: annealing schedule
-  local anneal_steps = 1000000
-  if self._step <= anneal_steps then
-    self._eps = 1.0 - self._step * (1.0 - 0.1) / anneal_steps
-  else
-    self._eps = 0.1
   end
 
   if 0 == self._step % _EXP_SAMPLING_FREQ then
     self._exp_cache:append({s, a, r, sp})
   end
 
-  print("exp_cache_size = "..self._exp_cache:size())
-  if not (0 == self._step % _TRAIN_FREQ and
-	  self._exp_cache:isFull()) then
-    return
-  end
-
-  print("Start training...")
-  local samples = mario_util.randSample(
-    _MINIBATCH_SIZE, self._exp_cache:size())
-  for i, sample in ipairs(samples) do
-    local exp = self._exp_cache:at(sample)
-    local s, a, r, sp = exp[1], exp[2], exp[3], exp[4]
-    local target_q = r
-    if sp then
-      target_q = r + _DISCOUNT_FACTOR * self:_maxQ(sp)
-    end
-    local q = self:_evalQ(s, a)  -- *
-    local dq = target_q - q
-    if dq ~= 0 then
-      local grad_output = torch.zeros(
-	#mario_util.joypad_input_code_from_simple):float()
-      if _ENABLE_CUDA then
-	grad_output = grad_output:cuda()
-      end
-      grad_output[a] = 1
-      self._dx:zero()
-      -- m.model:forward() has been called at *
-      self._m.model:backward(s, grad_output)
-      -- TODO: more sophisticated learning methods
-      self._x:add(self._dx * 1.0e-4 * dq)
-    end
+  if (0 == self._step % _TRAIN_FREQ and
+      self._exp_cache:isFull()) then
+    self:_doTraining()
   end
 end
 
@@ -254,6 +278,9 @@ function QModel:endEpoch()
     return self._epoch < 1
   end
 
+  self:_log("epoch = "..self._epoch..
+	    ", step = "..self._step..
+	    ", reward = "..self._current_reward)
   self:_saveModel()
   return true
 end
